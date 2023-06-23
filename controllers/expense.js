214c2eb17d5c1678df5a452db51fb5409593043a
poojabairagi3 <123469728+poojabairagi3@users.noreply.github.com>
2023-06-23 21:25:55 +0530

    Making Expense App Production ready
    Making Expense App Production ready

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,46 +1,27 @@
-// const sequelize = require('../util/database');
-const { NUMBER } = require('sequelize');
+const sequelize = require('../util/database');
 const Expense = require('../models/expense');
-const User=require('../models/user');
+const User = require('../models/user');
+
 
 exports.postExpense = async (req, res, next) => {
+    const t = await sequelize.transaction();
     try {
         // const user=req.user;
         const { amount, description, category } = req.body;
         console.log(req.user.id);
-            const data = await Expense.create({
-            amount: amount,
-            description: description,
-            category: category,
-            userId: req.user.id,
-        }).then(expense=>{
-            const totalExpense=parseInt(req.user.totalExpenses)+parseInt(amount)
-            console.log(totalExpense);
-            User.update({
-                totalExpenses:totalExpense
-            },{
-                where:{id:req.user.id}
-            }).then(async()=>{
-                // console.log(totalExpense);
-                res.status(200).json({expense})
-            })
-            .catch(async(err)=>{
-                console.log(err);
-            })
-        }).catch(async(err)=>{
-            console.log(err);
-        })
-        // console.log(data);
-
-        // return res.status(201).json({ details: data });
-
+        const expense = await Expense.create({ amount: amount, description: description, category: category, userId: req.user.id }, { transaction: t });
+        const totalExpense = parseInt(req.user.totalExpenses) + parseInt(amount);
+        await User.update({ totalExpenses: totalExpense }, { where: { id: req.user.id }, transaction: t })
+        await t.commit();
+        res.status(200).json({ expense })
     }
     catch (err) {
-        res.status(500).json(err);
-
+        await t.rollback();
+        console.log(err);
     }
-
 }
+
+
 
 exports.getExpense = async (req, res, next) => {
     try {
@@ -59,18 +40,24 @@ exports.getExpense = async (req, res, next) => {
 }
 
 exports.deleteExpense = async (req, res, next) => {
+    const t = await sequelize.transaction();    
     try {
         if (req.params.id == 'undefined') {
             console.log('ID is missing')
             return res.status(400).json({ err: 'Id is missing' })
         }
         const eId = req.params.id;
-        const amount=await Expense.findOne({where:{id:eId}})
-        await Expense.destroy({ where: { id: eId, userId: req.user.id } });
-        const totalExpenses=parseInt(req.user.totalExpenses)-parseInt(amount.amount);
-        await User.update({totalExpenses:totalExpenses},{where:{id:req.user.id}})
+        const amount = await Expense.findOne({ where: { id: eId } })
+        const data=await Expense.destroy({ where: { id: eId, userId: req.user.id }, transaction: t });
+        const totalExpenses = parseInt(req.user.totalExpenses) - parseInt(amount.amount);
+        await User.update({ totalExpenses: totalExpenses }, { where: { id: req.user.id }, transaction: t })
+        await t.commit();
+        if(data===0){
+            return res.status(400).json({message:"Expense does not belongs"})
+        }
         res.sendStatus(200);
     } catch (err) {
+        await t.rollback();
         console.log(err);
         res.status(500).json(err)
     }
